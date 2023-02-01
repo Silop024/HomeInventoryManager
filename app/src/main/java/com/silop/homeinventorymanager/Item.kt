e0828ca685ace6f9d37930b8373bab765eba7a99
Silop024 <jackmdwebb@hotmail.com>
2023-02-01 20:28:59 +0100

    Item view composables
    View of each item

---
+++ app/src/main/java/com/silop/homeinventorymanager/Item.kt
@@ -0,0 +1,3 @@
+package com.silop.homeinventorymanager
+
+data class Item(val id: Int, val name: String, var location: String, var last_needed: String)
--- app/src/main/java/com/silop/homeinventorymanager/MainActivity.kt
+++ app/src/main/java/com/silop/homeinventorymanager/MainActivity.kt
@@ -1,25 +1,31 @@
 package com.silop.homeinventorymanager
 
 import android.os.Bundle
+import android.widget.ListView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ScrollState
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
-import com.silop.homeinventorymanager.ui.theme.HomeInventoryManagerTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val test = listOf(
+            Item(0, "Banana", "Livingroom", "2023-01-31"),
+            Item(1, "Pinapple", "Livingroom", "2023-02-01"),
+            Item(2, "Cudez", "Livingroom", "Never"),
+            Item(3, "Paper", "Livingroom", "2023-02-01"),
+            Item(4, "TV", "Livingroom", "2023-01-23")
+        )
         setContent {
             Column(
                 modifier = Modifier
@@ -28,21 +34,40 @@ class MainActivity : ComponentActivity() {
                 horizontalAlignment = Alignment.CenterHorizontally,
                 verticalArrangement = Arrangement.Top
             ) {
-                Greeting(name = "Silop")
-                Greeting(name = "Jack")
-                Greeting(name = "Banana")
+                ItemList(items = test)
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ItemRow(item: Item) {
+    Row(
+        modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.spacedBy(10.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(text = item.name)
+        Text(text = item.location)
+        Text(text = item.last_needed)
+    }
+}
+
+@Composable
+fun ItemList(items: List<Item>) {
+    Column(
+        modifier = Modifier.verticalScroll(
+            state = ScrollState(0),
+            enabled = true,
+            reverseScrolling = false,
+        )
+    ) {
+        items.forEach { ItemRow(item = it) }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    Greeting("Silop")
+
 }
