a3967c186d4932cda4bb1b7d9aa0704d5c83ff10
Silop024 <jackmdwebb@hotmail.com>
2023-02-18 10:17:29 +0100

    Chaned styling properties

--- app/src/main/java/com/silop/homeinventorymanager/ui/theme/Type.kt
+++ app/src/main/java/com/silop/homeinventorymanager/ui/theme/Type.kt
@@ -1,28 +1,59 @@
 package com.silop.homeinventorymanager.ui.theme
 
 import androidx.compose.material.Typography
+import androidx.compose.ui.text.PlatformTextStyle
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.sp
+import com.silop.homeinventorymanager.R
 
-// Set of Material typography styles to start with
+val OpenSans = FontFamily(
+    Font(R.font.open_sans_semibold, FontWeight.Normal),
+    Font(R.font.open_sans_semibold_italic, FontWeight.Thin),
+    Font(R.font.open_sans_extrabold, FontWeight.ExtraBold)
+)
+
 val Typography = Typography(
-    body1 = TextStyle(
-        fontFamily = FontFamily.Default,
+    h1 = TextStyle(
+        fontFamily = OpenSans,
         fontWeight = FontWeight.Normal,
-        fontSize = 16.sp
-    )
-    /* Other default text styles to override
-    button = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.W500,
+        fontSize = 30.sp
+    ),
+    h2 = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 20.sp
+    ),
+    h3 = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Normal,
+        fontSize = 14.sp
+    ),
+    body1 = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Normal,
         fontSize = 14.sp
     ),
     caption = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.Thin,
         fontSize = 12.sp
+    ),
+    button = TextStyle(
+        fontFamily = OpenSans,
+        fontWeight = FontWeight.ExtraBold,
+        fontSize = 30.sp,
+        textAlign = TextAlign.Center,
+        platformStyle = PlatformTextStyle(
+            includeFontPadding = false
+        ),
+        lineHeightStyle = LineHeightStyle(
+            alignment = LineHeightStyle.Alignment.Center,
+            trim = LineHeightStyle.Trim.None
+        )
     )
-    */
 )
